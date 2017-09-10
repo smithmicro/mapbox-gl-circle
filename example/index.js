45aa6d337ed4f20f53752da4df118b2d15e62569
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-10 16:18:54 +0200

    Update example/index.js to do lots of funky stuff

--- example/index.js
+++ example/index.js
@@ -45,8 +45,10 @@ window.plainCircle0 = new MapboxCircle({lat: 39.982, lng: -75.345}, 250, nonEdit
 
 window.plainCircle1 = new MapboxCircle({lat: 39.983, lng: -75.344}, 300, nonEditableOpts).addTo(map);
 
-window.editableCircle1 = new MapboxCircle({lat: 39.984, lng: -75.349}, 300, editableOpts).addTo(map);
-window.editableCircle2 = new MapboxCircle({lat: 39.989, lng: -75.348}, 250, editableOpts).addTo(map);
+window.editableCircle1 = new MapboxCircle({lat: 39.984, lng: -75.349}, 300, editableOpts).addTo(map)
+    .setCenter({lat: 39.989, lng: -75.348}).setRadius(50);
+
+window.editableCircle2 = new MapboxCircle({lat: 39.974377, lng: -75.639449}, 25000, editableOpts).addTo(map);
 window.editableCircle3 = new MapboxCircle({lat: 39.980, lng: -75.340}, 225, editableOpts).addTo(map);
 
 window.plainCircle2 = new MapboxCircle({lat: 39.983, lng: -75.345}, 150, nonEditableOpts).addTo(map);
@@ -56,7 +58,7 @@ window.setTimeout(function () {
     window.editableCircle1.remove();
     window.editableCircle3.remove();
     window.setTimeout(function () {
-        window.editableCircle1.addTo(map);
+        window.editableCircle1.addTo(map).setCenter({lat: 39.984, lng: -75.349}).setRadius(300);
         window.editableCircle3.addTo(map);
     }, 1250);
 }, 2500);
@@ -64,8 +66,14 @@ window.setTimeout(function () {
 
 window.editableCircle2
     .on('radiuschanged', function (circleObj) {
+        const newRadius = circleObj.getRadius();
         // eslint-disable-next-line
         console.log('editableCircle2/radiuschanged', circleObj.getBounds());
+        window.setTimeout(function () {
+            if (newRadius === circleObj.getRadius()) {
+                circleObj.setRadius(newRadius * .99);
+            }
+        }, 1500);
     })
     .on('centerchanged', function (circleObj) {
         // eslint-disable-next-line
@@ -78,8 +86,14 @@ window.editableCircle2
 
 window.editableCircle3
     .on('radiuschanged', function (circleObj) {
+        const newRadius = circleObj.getRadius();
         // eslint-disable-next-line
         console.log('editableCircle3/radiuschanged', circleObj.getBounds());
+        window.setTimeout(function () {
+            if (newRadius === circleObj.getRadius()) {
+                circleObj.setRadius(newRadius * 1.01);
+            }
+        }, 1750);
     })
     .on('centerchanged', function (circleObj) {
         // eslint-disable-next-line
