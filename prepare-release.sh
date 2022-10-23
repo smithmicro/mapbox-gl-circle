36d9792f7ce3c3623549002826dc4af0e114eb4e
Mats Blomdahl <mats.blomdahl@gmail.com>
2022-10-23 10:30:46 +0200

    Ensure Changelog :pencil: has Next Version (#93)

--- prepare-release.sh
+++ prepare-release.sh
@@ -11,6 +11,7 @@ fi
 echo "Current git version: $(npm version from-git)"
 
 NEXT_VERSION=$(npm version "$VERSION_CAT")
+NEXT_VERSION=${NEXT_VERSION/v/}  # Drop the "v" from e.g. "v1.8.0".
 echo "Next $VERSION_CAT version: $NEXT_VERSION"
 
 if [[ ! $VERSION_CAT =~ ^pre ]]; then
